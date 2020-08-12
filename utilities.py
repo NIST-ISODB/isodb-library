d6eedcf3645b5b5e0b97fb1529c9d0a3c458e15d
Daniel W. Siderius <daniel.siderius@nist.gov>
2020-08-12 17:00:07 -0400

    ci: added functions in utilities to regenerate adsorbent, adsorbate, and bibliography libraries

--- utilities.py
+++ utilities.py
@@ -177,6 +177,107 @@ def regenerate_library():
     DOI_mapping.close()
 
 
+@cli.command("regenerate_adsorbents")
+def regenerate_adsorbents():
+    """Generate the entire ISODB library from the API"""
+    # Create the JSON Library folder if necessary
+    if not os.path.exists(JSON_FOLDER):
+        os.mkdir(JSON_FOLDER)
+
+    # Create subfolder for Adsorbents if necessary
+    Adsorbent_folder = JSON_FOLDER + "/Adsorbents"
+    if not os.path.exists(Adsorbent_folder):
+        os.mkdir(Adsorbent_folder)
+
+    # Generate a list of adsorbents from the MATDB API
+    url = API_HOST + "/matdb/api/materials.json"
+    adsorbents = json.loads(requests.get(url, headers=HEADERS).content)
+    print(len(adsorbents), "Adsorbent Material Entries")
+
+    # Extract each adsorbent in full form
+    for (material_count, adsorbent) in enumerate(adsorbents):
+        filename = adsorbent["hashkey"] + ".json"
+        url = API_HOST + "/matdb/api/material/" + filename
+        print(url)
+        adsorbent_data = json.loads(requests.get(url, headers=HEADERS).content)
+        # pprint.pprint(adsorbent_data)
+        # Write to JSON
+        json_writer(Adsorbent_folder + "/" + filename, adsorbent_data)
+        if material_count % 100 == 0:
+            time.sleep(5)  # slow down API calls to not overwhelm the server
+
+
+@cli.command("regenerate_adsorbates")
+def regenerate_adsorbates():
+    """Generate the entire ISODB library from the API"""
+    # Create the JSON Library folder if necessary
+    if not os.path.exists(JSON_FOLDER):
+        os.mkdir(JSON_FOLDER)
+
+    # Create subfolder for Adsorbates if necessary
+    Adsorbate_folder = JSON_FOLDER + "/Adsorbates"
+    if not os.path.exists(Adsorbate_folder):
+        os.mkdir(Adsorbate_folder)
+
+    # Generate a list of adsorbents from the MATDB API
+    url = API_HOST + "/isodb/api/gases.json"
+    adsorbates = json.loads(requests.get(url, headers=HEADERS).content)
+    print(len(adsorbates), "Adsorbate Species Entries")
+
+    # Extract each adsorbate in full form
+    for (adsorbate_count, adsorbate) in enumerate(adsorbates):
+        filename = adsorbate["InChIKey"] + ".json"
+        url = API_HOST + "/isodb/api/gas/" + filename
+        print(url)
+        try:
+            adsorbate_data = json.loads(requests.get(url, headers=HEADERS).content)
+            # pprint.pprint(adsorbate_data)
+            # Write to JSON
+            json_writer(Adsorbate_folder + "/" + filename, adsorbate_data)
+        except:
+            print("ERROR: ", adsorbate)
+        # if adsorbate_count > 5: break
+        if adsorbate_count % 100 == 0:
+            time.sleep(5)  # slow down API calls to not overwhelm the server
+
+
+@cli.command("regenerate_bibliography")
+def regenerate_bibliography():
+    """Generate the entire ISODB library from the API"""
+    # Create the JSON Library folder if necessary
+    if not os.path.exists(JSON_FOLDER):
+        os.mkdir(JSON_FOLDER)
+
+    # Create subfolder for Adsorbates if necessary
+    Biblio_folder = JSON_FOLDER + "/Bibliography"
+    if not os.path.exists(Biblio_folder):
+        os.mkdir(Biblio_folder)
+
+    # Generate a list of adsorbents from the MATDB API
+    url = API_HOST + "/isodb/api/biblios.json"
+    bibliography = json.loads(requests.get(url, headers=HEADERS).content)
+    print(len(bibliography), "Bibliography Entries")
+
+    # Extract each paper in full form
+    for (biblio_count, biblio) in enumerate(bibliography):
+        doi = biblio["DOI"]
+        url = API_HOST + "/isodb/api/biblio/" + doi + ".json"
+        print(url)
+        try:
+            biblio_data = json.loads(requests.get(url, headers=HEADERS).content)[
+                0
+            ]  # look at the API call here
+            pprint.pprint(biblio_data)
+            # Write to JSON
+            # json_writer(Adsorbate_folder+'/'+filename,adsorbate_data)
+        except:
+            print("ERROR: ", doi)
+        break
+        # if biblio_count > 5: break
+        if biblio_count % 100 == 0:
+            time.sleep(5)  # slow down API calls to not overwhelm the server
+
+
 @cli.command("git_log")
 def git_log():
     """parse the git log"""
