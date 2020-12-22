cca52e82ca75d2e6f3d4ee017523348555e41ac8
Daniel W. Siderius <daniel.siderius@nist.gov>
2020-12-22 17:27:36 -0500

    ci: another mod to validate.py to satisfy BLACK

--- .github/validate.py
+++ .github/validate.py
@@ -39,7 +39,7 @@ def validate_doi_mapping():
     """Check consistency of DOI_mapping.csv with Library/ folder."""
     doi_mapping = pd.read_csv(DOI_MAPPING_PATH, sep=", ", engine="python")
     doi_mapping.columns = [
-        column.replace(" ", "").replace('"','') for column in doi_mapping.columns
+        column.replace(" ", "").replace('"', "") for column in doi_mapping.columns
     ]
     doi_stubs = doi_mapping["DOI_Stub"].str.lower()
     doi_folders = [f.name.lower() for f in os.scandir(JSON_FOLDER) if f.is_dir()]
